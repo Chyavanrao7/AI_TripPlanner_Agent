9d83cbe0318a3e59fbdd131ade6a66f3602b04ad
Chyavan Rao <145827567+Chyavanrao7@users.noreply.github.com>
2025-09-12 12:05:41 +0530

    Update next.config.mjs

--- frontfront/next.config.mjs
+++ frontfront/next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   env: {
-    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8000',
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || 'agentbackend-production.up.railway.app',
   },
   eslint: {
     ignoreDuringBuilds: true,
